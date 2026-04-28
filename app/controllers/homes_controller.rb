858404eeb85527d4becc34839c25961e4fd51e7c
hypocrite-hero <kqiantian77@gmail.com>
2026-04-28 19:53:19 +0900

    一覧表示の見た目を変更

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,8 @@
 class HomesController < ApplicationController
   def index
     if user_signed_in?
+      # ログインしている場合、そのユーザーのキャラクターとルーティンを取得
+      @character = current_user.character
       @routines = current_user.routines.order(created_at: :desc)
     end
   end
